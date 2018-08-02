322196bd9cb0df6519a0aad6d242ef033cf4127e
katnav <katherinenavarro@gmail.com>
2018-08-02 17:28:07 -0400

    totals work

--- scripts/main.js
+++ scripts/main.js
@@ -1,73 +1,65 @@
-//$('table').on('mouseup keyup', 'input[type=number]', () => calculateTotals());
-
-
-
 
 
 $(document).ready(function() {
 
+    //add a new row when button (.addItem) is clicked
+    $('.addItem').on('click', function(e) {
+        e.preventDefault();
+
+});
 
 
-$('form').on('mouseup keyup', 'input[type=number]', () => calculateItemPrice());
+function formatCurrency(amount) {
+    return `$${Number(amount).toFixed(2)}`;
+};
 
-    function calculateItemPrice() {
+$('.addItem').on('click', () => {
+        let newItem = "<tr class='item'><td><input placeholder='Enter item description' name='itemName' autofocus></td> <td>$<input type='number' name='itemUnitPrice'></td> <td><input type='number' name='itemQty'></td> <td class='itemPrice'>0.00</td> </tr>";
+        $('table#invoiceDetails').append(newItem);
+        $('input[name=itemName]').focus();
 
-        const itemQty = $('input[name=itemQty').val();
-        const itemUnitPrice = $('input[name=itemUnitPrice]').val();
-        const itemPrice = (itemQty * itemUnitPrice);
-        //console.log(itemPrice);
-        $('.itemPrice').text(formatCurrency(itemPrice));
+    });
+
+
+
+$('table#invoiceDetails').on('mouseup keyup', 'input[type=number]', () => calculateTotals());
+
+    //calculates all
+    function calculateTotals() {
+        let itemPrices =  $('.item').map((index, val) => calculateItemPrice(val)).get();
+
+        let subtotal = itemPrices.reduce((a, v) => a + Number(v), 0);
+        $('.subtotal').text(formatCurrency(subtotal));
+
+        const tax = .13;
+        let taxAmount = subtotal * tax;
+        let total = subtotal + taxAmount;
+        $('.taxAmount').text(formatCurrency(taxAmount));
+        $('.total').text(formatCurrency(total));
+
+        return subtotal;
+        return taxAmount;
+
 
     };
 
+    //calculates item price (row)
+    function calculateItemPrice() {
+        let itemQty = $('input[name=itemQty]').val();
+        let itemUnitPrice = $('input[name=itemUnitPrice]').val();
+        let itemPrice = (itemQty * itemUnitPrice);
+        console.log(itemPrice);
+        $('.itemPrice').text(formatCurrency(itemPrice));
 
-$('.addItem').click(function(e) {
-    e.preventDefault();
-        const newItem = "<tr class='item'><td><input placeholder='Enter item description' name='itemName' autofocus></td> <td>$<input type='number' name='itemUnitPrice'></td> <td><input type='number' name='itemQty'></td> <td class='itemPrice'></td> </tr>";
-        $('table#invoiceDetails').append(newItem);
-    });
+        return itemPrice;
+
+    };
+
 
 });
 
 
 
-function formatCurrency(amount) {
-    return `$${Number(amount).toFixed(2)}`;
-}
-
-// $(document).ready(function(){
-//         $(".add-row").click(function(){
-//             var name = $("#name").val();
-//             var email = $("#email").val();
-//             var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + email + "</td></tr>";
-//             $("table tbody").append(markup);
-//         });
-
-//         // Find and remove selected table rows
-//         $(".delete-row").click(function(){
-//             $("table tbody").find('input[name="record"]').each(function(){
-//                 if($(this).is(":checked")){
-//                     $(this).parents("tr").remove();
-//                 }
-//             });
-//         });
-//     });
 
 
-// $('.btn-add-row').on('click', () => {
-//   const $lastRow = $('.item:last');
-//   const $newRow = $lastRow.clone();
-
-//   $newRow.find('input').val('');
-//   $newRow.find('td:last').text('$0.00');
-//   $newRow.insertAfter($lastRow);
-
-//   $newRow.find('input:first').focus();
-// });
-
-// function calculateTotals() {
-//   const subtotals = $('.item').map((idx, val) => calculateSubtotal(val)).get();
-//   const total = subtotals.reduce((a, v) => a + Number(v), 0);
-//   $('.total td:eq(1)').text(formatAsCurrency(total));
-// }
 
