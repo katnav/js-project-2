1c0a2ecfd4531afaaec35bd3851976bd912935a3
katnav <katherinenavarro@gmail.com>
2018-08-03 22:25:49 -0400

    add row; delete row; calculate total

--- scripts/main.js
+++ scripts/main.js
@@ -3,29 +3,49 @@
 $(document).ready(function() {
 
     //add a new row when button (.addItem) is clicked
-    $('.addItem').on('click', function(e) {
+    $('.add-item').on('click', function(e) {
         e.preventDefault();
 
 });
+
+// row background color does not work
+//     $('#invoice-details tr').on('click', function(e) {
+//         $('#invoice-details tr').removeClass('selected');
+//     $(this).addClass('selected');
+// });
+
+//delete row
+
+$("#invoice-details").on('click','.delete',function(){
+       $(this).closest('tr').remove();
+       calculateTotals();
+     });
+
+
+
 
 
 function formatCurrency(amount) {
     return `$${Number(amount).toFixed(2)}`;
 };
 
-$('.addItem').on('click', () => {
-        let newItem = "<tr class='item'><td><input placeholder='Enter item description' name='itemName' autofocus></td> <td>$<input type='number' name='itemUnitPrice'></td> <td><input type='number' name='itemQty'></td> <td class='itemPrice'>0.00</td> </tr>";
-        $('table#invoiceDetails').append(newItem);
+$('.add-item').on('click', () => {
+        let newItem = "<tr class='item'><td><input placeholder='Item Name' name='itemName' autofocus></td> <td>$<input type='number' name='itemUnitPrice' placeholder='$0.00'></td> <td><input type='number' name='itemQty' placeholder='0'></td> <td class='item-price'>$0.00</td><td><div class='delete'><i class='fas fa-minus-circle'></i><div></td> </tr>";
+        $(newItem).find('input[name=itemPrice]').val('');
+        $('#invoice-details').append([newItem]);
         $('input[name=itemName]').focus();
 
     });
 
 
+//calculate row price as you input value
+$('#invoice-details').on('mouseup keyup', 'input[type=number]', () => calculateTotals());
 
-$('table#invoiceDetails').on('mouseup keyup', 'input[type=number]', () => calculateTotals());
 
-    //calculates all
+    //calculate subtotal, tax amount, and total
     function calculateTotals() {
+
+
         let itemPrices =  $('.item').map((index, val) => calculateItemPrice(val)).get();
 
         let subtotal = itemPrices.reduce((a, v) => a + Number(v), 0);
@@ -34,7 +54,7 @@ $('table#invoiceDetails').on('mouseup keyup', 'input[type=number]', () => calcul
         const tax = .13;
         let taxAmount = subtotal * tax;
         let total = subtotal + taxAmount;
-        $('.taxAmount').text(formatCurrency(taxAmount));
+        $('.tax-amount').text(formatCurrency(taxAmount));
         $('.total').text(formatCurrency(total));
 
         return subtotal;
@@ -43,13 +63,15 @@ $('table#invoiceDetails').on('mouseup keyup', 'input[type=number]', () => calcul
 
     };
 
+
+
     //calculates item price (row)
-    function calculateItemPrice() {
-        let itemQty = $('input[name=itemQty]').val();
-        let itemUnitPrice = $('input[name=itemUnitPrice]').val();
+    function calculateItemPrice(item) {
+        let itemQty = $('input[name=itemQty]', item).val();
+        let itemUnitPrice = $('input[name=itemUnitPrice]', item).val();
         let itemPrice = (itemQty * itemUnitPrice);
         console.log(itemPrice);
-        $('.itemPrice').text(formatCurrency(itemPrice));
+        $('.item-price', item).text(formatCurrency(itemPrice));
 
         return itemPrice;
 
@@ -57,8 +79,6 @@ $('table#invoiceDetails').on('mouseup keyup', 'input[type=number]', () => calcul
 
 
 });
-
-
 
 
 
